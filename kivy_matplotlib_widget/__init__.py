df579956fbe98067cd0e3ca0bd54c18df8fbf72b
mp-007 <mp007ets@gmail.com>
2023-01-19 20:41:50 -0500

    add version 0.3.1

--- kivy_matplotlib_widget/__init__.py
+++ kivy_matplotlib_widget/__init__.py
@@ -5,6 +5,6 @@ kivy.require("1.11.1")
 
 import kivy_matplotlib_widget.factory_registers  # NOQA
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __description__ = "A Matplotlib interactive widget for kivy"
 __author__ = "mp-007"
