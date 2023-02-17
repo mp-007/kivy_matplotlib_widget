c4a38da6d261f538e6da82c8110e0377d5252a3d
mp-007 <mp007ets@gmail.com>
2023-02-17 09:41:45 -0500

    bump version 0.4.0

--- kivy_matplotlib_widget/__init__.py
+++ kivy_matplotlib_widget/__init__.py
@@ -5,6 +5,6 @@ kivy.require("2.0.0")
 
 import kivy_matplotlib_widget.factory_registers  # NOQA
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __description__ = "A Matplotlib interactive widget for kivy"
 __author__ = "mp-007"
