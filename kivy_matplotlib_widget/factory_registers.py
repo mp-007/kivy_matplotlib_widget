4f73bc282cea04232bba81cf07f0f868c1ab6f01
mp-007 <mp007ets@gmail.com>
2024-01-03 09:50:08 -0500

    register MatplotFigureSubplot widget

--- kivy_matplotlib_widget/factory_registers.py
+++ kivy_matplotlib_widget/factory_registers.py
@@ -7,6 +7,7 @@ r("MatplotFigureScatter", module="kivy_matplotlib_widget.uix.graph_widget_scatte
 r("MatplotFigure3D", module="kivy_matplotlib_widget.uix.graph_widget_3d")
 r("MatplotFigureGeneral", module="kivy_matplotlib_widget.uix.graph_widget_general")
 r("MatplotFigureTwinx", module="kivy_matplotlib_widget.uix.graph_widget_twinx")
+r("MatplotFigureSubplot", module="kivy_matplotlib_widget.uix.graph_subplot_widget")
 r("MatplotNavToolbar", module="kivy_matplotlib_widget.uix.navigation_bar_widget")
 r("LegendRv", module="kivy_matplotlib_widget.uix.legend_widget")
 r("LegendRvHorizontal", module="kivy_matplotlib_widget.uix.legend_widget")
